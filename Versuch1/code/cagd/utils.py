aad0d89437be5b371554ae4c8b9f2ebbc25dc156
Niels <ubqyr@student.kit.edu>
2024-05-07 17:15:36 +0200

    Niels solution

--- Versuch1/code/cagd/utils.py
+++ Versuch1/code/cagd/utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from cagd.vec import Vec2
 
+import numpy as np
+
 
 # Solves the system of linear equations Ax = res
 # where A is a tridiagonal matrix with diag2 representing the main diagonal
@@ -11,6 +13,25 @@ from cagd.vec import Vec2
 def solve_tridiagonal_equation(diag1, diag2, diag3, res):
     assert (len(diag1) == len(diag2) == len(diag3) == len(res))
     solution = None
+
+    dim = len(res)
+
+    diag1[0] = 0
+    diag3[-1] = 0
+
+    v = [0]
+    y = [Vec2(0, 0)]
+
+    for i in range(1, dim + 1):
+        z = 1. / (diag2[i - 1] - diag1[i - 1] * v[i - 1])
+        v.append(z * diag3[i - 1])
+        y.append(z * (res[i - 1] - diag1[i - 1] * y[i - 1]))
+
+    solution = [Vec2(0, 0)] * dim
+    solution[-1] = y[-1]
+    for i in range(dim - 2, -1, -1):
+        solution[i] = y[i + 1] - v[i + 1] * solution[i + 1]
+
     return solution
 
 
