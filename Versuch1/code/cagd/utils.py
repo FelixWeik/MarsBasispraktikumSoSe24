11d5185da04773dfbd2d21d45de20c5a62f2eb4b
Celia Djouadi <uzoer@student.kit.edu>
2024-05-25 20:49:03 +0000

    implementation of solve_tridiagonal_equation

--- Versuch1/code/cagd/utils.py
+++ Versuch1/code/cagd/utils.py
@@ -43,5 +43,48 @@ def solve_tridiagonal_equation(diag1, diag2, diag3, res):
 # diag1[i], diag2[i] and diag3[i] are located on the same row of A
 def solve_almost_tridiagonal_equation(diag1, diag2, diag3, res):
     assert (len(diag1) == len(diag2) == len(diag3) == len(res))
-    solution = None
+
+    n = len(diag1)
+
+    # Initialize the vectors
+    v = [0.0] * n
+    y = [Vec2(0.0, 0.0)] * n
+    s = [0.0] * n
+    z = [0.0] * n
+
+    # Forward Elimination
+    z[0] = 1.0 / diag2[0]
+    y[0] = res[0] * z[0]
+    v[0] = -z[0] * diag3[0]
+    s[0] = -diag1[0] * z[0]
+
+    for k in range(1, n):
+        z[k] = 1.0 / (diag2[k] + diag1[k] * v[k - 1])
+        v[k] = -z[k] * diag3[k]
+        y[k] = z[k] * (res[k] - diag1[k] * y[k - 1])
+        s[k] = -diag1[k] * s[k - 1] * z[k]
+
+    # Back substitution
+    t = [0.0] * n
+    w = [Vec2(0.0, 0.0)] * n
+
+    t[-1] = 1.0
+    w[-1] = Vec2(0.0, 0.0)
+
+    for k in range(n-2, -1, -1):
+        t[k] = v[k] * t[k+1] + s[k]
+        w[k] = v[k] * w[k+1] + y[k]
+
+    # Calculate xn
+    a = diag1[-1] * t[-2] + diag3[-1] * t[0] + diag2[-1]
+    xn_x = (res[-1].x - diag1[-1] * w[-2].x - diag3[-1] * w[0].x) / a
+    xn_y = (res[-1].y - diag1[-1] * w[-2].y - diag3[-1] * w[0].y) / a
+    xn = Vec2(xn_x, xn_y)
+
+    solution = [Vec2(0.0, 0.0)] * n
+    solution[-1] = Vec2(xn, xn)
+
+    for k in range(n-1, -1, -1):
+        solution[k] = Vec2(t[k] * xn.x + w[k].x, t[k] * xn.y + w[k].y)
+
     return solution
